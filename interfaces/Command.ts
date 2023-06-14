ab1c02e579df57e1c248b32367e0653433a3dc46
tnkbang <trieukhanhbang123@gmail.com>
2023-06-14 23:48:16 +0700

    add data option in command interface

--- interfaces/Command.ts
+++ interfaces/Command.ts
@@ -1,8 +1,15 @@
 import { SlashCommandBuilder } from "discord.js";
 
+export interface DataOptions {
+  name: string;
+  sname?: string;
+  type?: string;
+  description: string;
+}
+
 export interface Command {
   permissions?: string[];
   cooldown?: number;
-  data?: SlashCommandBuilder;
+  data?: SlashCommandBuilder | DataOptions;
   execute(...args: any): any;
 }
