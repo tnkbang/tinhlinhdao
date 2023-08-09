0e81189a52a4fa5041f2f8b9399f8f3bc76a9f93
tnkbang <tnkbang@cusc.ctu.edu.vn>
2023-08-09 18:54:31 +0900

    temporary fix playlist with mix video

--- structs/Playlist.ts
+++ structs/Playlist.ts
@@ -1,7 +1,8 @@
-import youtube, { Playlist as YoutubePlaylist } from "youtube-sr";
+import youtube, { Video, Playlist as YoutubePlaylist } from "youtube-sr";
 import { config } from "../utils/config";
 import { Song } from "./Song";
 import { playlistPattern } from "../utils/patterns";
+import { i18n } from "../utils/i18n";
 
 export class Playlist {
   public data: YoutubePlaylist;
@@ -26,17 +27,26 @@ export class Playlist {
     var reg = new RegExp("[?]v=([a-z0-9_]+)", "i");
     var match = reg.exec(url);
 
-    if (match) search = url = match[1]
+    if (match) search = match[1]
 
-    const urlValid = playlistPattern.test(url);
-    let playlist;
+    const urlValid = playlistPattern.test(search);
+    let playlist = new YoutubePlaylist()
 
     if (urlValid) {
-      playlist = await youtube.getPlaylist(url);
+      playlist = await youtube.getPlaylist(search);
     } else {
       const result = await youtube.searchOne(search, "playlist");
+      if (result) playlist = await youtube.getPlaylist(result.url!);
+      else {
+        //can't find playlist then get from id video
+        playlist.title = i18n.__("playlist.titleMixed");
+        playlist.url = url;
 
-      playlist = await youtube.getPlaylist(result.url!);
+        //get video from mix playlist
+        (await youtube.search(search, { type: 'video', limit: 1 })).map(v => {
+          playlist.videos.push(new Video(v))
+        })
+      }
     }
 
     return new this(playlist);
