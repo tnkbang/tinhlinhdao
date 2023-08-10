0c1e22432af616060610575f5638c529ded032bb
tnkbang <tnkbang@cusc.ctu.edu.vn>
2023-08-10 16:10:20 +0900

    change logic search playlist

--- structs/Playlist.ts
+++ structs/Playlist.ts
@@ -1,7 +1,7 @@
 import youtube, { Playlist as YoutubePlaylist } from "youtube-sr";
 import { config } from "../utils/config";
 import { Song } from "./Song";
-import { playlistPattern } from "../utils/patterns";
+import { isURL, playlistPattern } from "../utils/patterns";
 import { i18n } from "../utils/i18n";
 
 export class Playlist {
@@ -32,22 +32,35 @@ export class Playlist {
     const urlValid = playlistPattern.test(search);
     let playlist = new YoutubePlaylist()
 
-    if (urlValid) {
-      playlist = await youtube.getPlaylist(search);
-    } else {
-      const pls = await youtube.searchOne(search, "playlist");
-      if (pls) playlist = await youtube.getPlaylist(pls.url!);
-      else {
-        //can't find playlist then get from id video
-        playlist.title = i18n.__("playlist.titleMixed");
-        playlist.url = url;
-
-        //get video from mix playlist
-        const video = await youtube.searchOne(search, 'video');
-        if (video) playlist.videos.push(await youtube.getVideo(video.url))
-      }
-    }
+    if (urlValid) playlist = await youtube.getPlaylist(search);
+    else playlist = await this.getVideoMix(url, search)
 
     return new this(playlist);
+  }
+
+  //get video when search query not find playlist
+  private static async getVideoMix(url: string, search: string) {
+    //has playlist when search
+    const pls = await youtube.searchOne(search, "playlist");
+    if (pls) return await youtube.getPlaylist(pls.url!);
+
+    //can't find playlist when search
+    let playlist = new YoutubePlaylist()
+    playlist.title = i18n.__("playlist.titleMixed");
+
+    //search query is mix playlist url
+    if (isURL.test(url)) {
+      playlist.url = url;
+      const video = await youtube.searchOne(search, 'video');
+      if (video) playlist.videos.push(await youtube.getVideo(video.url))
+    }
+    else {
+      //search query is string then search 5 videos
+      playlist.url = `https://www.youtube.com/results?search_query=${search.replace(' ', '+')}`;
+      const videos = await youtube.search(search, { type: 'video', limit: 5 })
+      if (videos) playlist.videos.push(...videos)
+    }
+
+    return playlist
   }
 }
